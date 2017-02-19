b3d18eeaa1cf862bc482424767659287ba730c04
dang@primo <dan.garton@gmail.com>
2017-02-19 19:48:36 +0000

    pre-ir scraper working for archive pages

--- irScrape/spiders/pre_ir_scrape.py
+++ irScrape/spiders/pre_ir_scrape.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy.exporters import JsonItemExporter
 
 class Tracklist(scrapy.Item):
@@ -12,113 +13,101 @@ class Tracklist(scrapy.Item):
 class pre_IRScrapeSpider(scrapy.Spider):
     name = "pre_ir_scrape"
     allowed_domains = ["www.bbc.co.uk"]
-    # eg start_urls = ['http://www.bbc.co.uk/programmes/b01fm4ss/broadcasts/2016/01']
-    # start_urls = ['http://www.bbc.co.uk/programmes/b01fm4ss/broadcasts/2016/01']
-    # for May 2002 - Oct 2009 - DIFFERENT CRAWLER
-    start_urls = ['http://www.bbc.co.uk/radio1/gillespeterson/tracklistings200']
+    # for May 2002 - Oct 2009
+    start_urls = ['http://www.bbc.co.uk/radio1/gillespeterson/tracklistings2008.shtml']
     # start_urls = ['http://www.bbc.co.uk/radio1/gillespeterson/tracklistings200[2-9].shtml']
-    
-    # different crawler...
-    # for Nov 2009 - Mar 2012
-    #start_urls = ['http://www.bbc.co.uk/programmes/b006wq8d/broadcasts']
-    # for April 2012 - current
-    #start_urls = ['http://www.bbc.co.uk/programmes/b01fm4ss/broadcasts/']
 
-    def start_requests(self):
-        for year in range(2,10):
-            url = self.start_urls[0] + str(year) + ".shtml"
-            yield scrapy.Request(url, callback=self.parse)
+#    def start_requests(self):
+#        for year in range(2,10):
+#            url = self.start_urls[0] + str(year) + ".shtml"
+#            yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        for show in response.css("div.br-box-page.programmes-page ol li"):
-            show_url = show.css("div div div.broadcast__programme.grid.three-quarters.bpb2-five-sixths.bpw-five-sixths div div.programme__body h4.programme__titles a::attr(href)").extract_first()
-            print ("SHOW_URL:" + show_url)
+        for show in response.css("#tracklisting_wrapper div div"):
+            p_tracklist = Tracklist()
+            p_tracklist['showurl'] = show.css("p.date a::attr(href)").extract_first()
+            p_tracklist['showdate'] = p_tracklist['showurl']
+            p_tracklist['showname'] =  show.css("p.info::text").extract_first()
+            show_url = p_tracklist['showurl']
             if show_url is not None:
                 show_url = response.urljoin(show_url)
-                yield scrapy.Request(show_url, callback=self.parse_show)
-
-    def parse_show(self, response):
-        p_tracklist = Tracklist()
-    
-        pr_showdate = response.css("#programmes-main-content div.b-g-p.no-margin-vertical div div.grid.bpw2-one-third.bpe-one-third.map__column.map__column--2.map__column--last div div div.programme__body.programme__body--flush div.broadcast-event__time.beta span.broadcast-event__date.text-base.timezone--date::text").extract_first()
-        p_tracklist['showdate'] = pr_showdate
-        p_tracklist['showname'] = response.css("#programmes-main-content div.b-g-p.no-margin-vertical div div.grid.bpw2-two-thirds.bpe-two-thirds.map__column.map__column--first div div.island div:nth-child(1) h1::text").extract()
-        
-        desc_list = []
-        for paragraph in response.css("#programmes-main-content > div.b-g-p.no-margin-vertical > div > div.grid.bpw2-two-thirds.bpe-two-thirds.map__column.map__column--first > div > div.island > div.grid-wrapper > div.grid.bpw-two-thirds.bpe-three-quarters > div > div > div.map__intro__synopsis.centi"):
-            desc_list.append(paragraph.css("p::text").extract())
-            #FIXME random extra bit in pages like 23.1.2016 - but hardly anything really .....
-#            extra_bit = paragraph.css("p span::text").extract()
-#            if extra_bit is not None:
-#                desc_list.append(extra_bit)
-        p_tracklist['showdesc'] = desc_list
-        
-    # FIXME - no need to loop here
-        for playlist in response.css("#segments"):
-            playlist_url = playlist.css("div.lazy-module.lazy-module--loading--loader::attr(data-lazyload-inc)").extract_first()
-            if playlist_url is not None:
-                playlist_url = response.urljoin(playlist_url)
-                play_req = scrapy.Request(playlist_url, callback=self.parse_playlist)
+                play_req = scrapy.Request(show_url, callback=self.parse_show)
                 play_req.meta['tracklist'] = p_tracklist
                 yield play_req
-        
-        file = open('%s_tracklist.json' % pr_showdate, 'w+b')
-        jsonExporter = JsonItemExporter(file)
-        jsonExporter.export_item(p_tracklist)
 
-
-    def parse_playlist(self, response):
+    def parse_show(self, response): 
         tracklist = response.meta['tracklist']
         t_list = []
         index = 1
-        
-        # FIXME - need to switch on "sub-class" of li element
-        for track in response.css("#segments div.component__body.br-box-page div ul li"):
-            # EXAMPLES 
-            # kode9 ... http://www.bbc.co.uk/programmes/b00q8xs9
-            # ed motta .. http://www.bbc.co.uk/programmes/b06whk5b
+        for track in response.css("#main-rm div font"):
+        #for track in response.css("#main-rm div font::text"):
+            #print ("TRAAAAACK: %s" % track)
+            #print ("TRACCK SUBS: %s " % track.extract())
+            for line in track.extract().split("\r\n"):
+                for brline in line.split("<br>"):
+                    brline = brline.strip()
+                    # remove all kinds of tags
+                    brline = re.sub('<.*font.*?>', '', brline) 
+                    brline = re.sub('<.*strong.*?>', '', brline) 
+                    brline = re.sub('<.?b.?>', '', brline)
+                    brline = re.sub('</a>', '', brline)
+                    brline = re.sub('<.*em.*?>', '', brline)
+                    brline = re.sub('<.*u.*?>', '', brline)
+                    # remove time line
+                    brline = re.sub('0?[23][\.\:]00', '', brline)
+                    
+                    if brline != "":
+                        # assume it's a track if we have: asdasdasdasd-'asdadasdasd'....
+                        m = re.match("(.*?)([-–] | [-–])(.*)$", brline)
+                        if m is not None: # we have a track
+                            artist = m.group(1)
+                            tracktitle = m.group(3)
+                            track_entry = {
+                                'index': index,
+                                'artist': artist,
+                                'trackname': tracktitle
+                            }
+                            # match label if we can, last pair of brackets (could be a first re-edit bracket set, say)
+                            l = re.match("^(.*)(\(.*\)){1}", tracktitle)
+                            if l is not None:
+                                track_entry['label'] = l.group(2)
+                                track_entry['trackname'] = l.group(1) 
+                        else: # it's a chapter title
+                            track_entry = {
+                                'index': index,
+                                'blurb': brline
+                            }
+                            # FIX for this brokenness:
+                                # Valley' (Intro) (Sonar Kollektiv)
+                                # Switch &amp; Rusko 'Untitled' (White)"
+                                # Baby Charles ‘Trading Water’ (RK Record Kicks)
+#                            b = re.match("(.*?)([‘'].*['’]).*(\(.*\))", brline)
+#                            if b is not None:
+#                                track_entry = {
+#                                    'index': index,
+#                                    'artist': group(1),
+#                                    'trackname': group(2),
+#                                    'label': group(3)
+#                                }
+                        
+                        # final clean...
+                        # remove ‘’' from trackname
+                        if 'trackname' in track_entry:
+                            r = re.match(".*[‘’'].*", track_entry['trackname'])
+                            if r is not None:
+                                track_entry['trackname'] = re.sub('[‘’\']', '', track_entry['trackname'])
+                            track_entry['trackname'] = track_entry['trackname'].strip()
+                        
+                        # remove () from label
+                        if 'label' in track_entry:
+                            l = re.match(".*[()].*", track_entry['label'])
+                            if l is not None:
+                                track_entry['label'] = re.sub('[\(\)]', '', track_entry['label'])
+                            track_entry['label'] = track_entry['label'].strip()    
 
-            item_class = track.css("li::attr(class)").extract()
-
-            # The main itemtype, Tracklist entry
-            if item_class[0].startswith("segments-list__item segments-list__item--music"):
-                track_entry = {
-                    'index': index,
-                    #FIXME fix "space ape" tune -  also as in http://www.bbc.co.uk/programmes/b00q8xs9 kode9: (space ape tune) #segments > div.component__body.br-box-page > div > ul > li:nth-child(9) > ul > li:nth-child(3) > div > div.segment__content.segment--withbuttons > div.segment__track > p > span
-                    'artist': track.css("h3 span span.artist::text").extract_first(),
-                    #FIXME check for "feat: " also in trackname
-                    'trackname': track.css("p span::text").extract_first(),
-                    'label': track.css("ul li span::text").extract_first()
-                }
-                # Fix for h4
-                if track_entry['artist'] is None:
-                    track_entry['artist'] = track.css("h4 span span.artist::text").extract_first()
-                #print ("TRACK: %s" % track_entry)
-                t_list.append(track_entry)
-                index = index+1
-            
-            # For when the list entry is a Guest mix, or Words and Music section. Just grab title. Getting the full length of section too complex for now
-            if item_class[0].startswith("segments-list__item segments-list__item--group"):
-                #print ("SUBMIX NAME: %s" % track.css("h3::text").extract_first())
-                submix_entry = {
-                    'index': index,
-                    'title': track.css("h3::text").extract_first()
-                }
-                t_list.append(submix_entry)
-                index = index+1
-            
-            # For another itemtype, when it's a separate words-only segment ... quite rare but it happens....
-            if item_class[0].startswith("segments-list__item.segments-list__item--speech"):
-                speechbit_entry = {
-                    'index': index,
-                    'blurb': track.css("h4::text").extract(),
-                }
-                t_list.append(speechbit_entry)
-                #print ("SPEECHBIT: %s" % speechbit_entry)
-                index = index+1
-                
-            # end track iteration
+                        t_list.append(track_entry)
+                        index = index + 1
         tracklist['showtrax'] = t_list
-        #print ("TLIST...%s" % t_list)
         return tracklist
         
+
